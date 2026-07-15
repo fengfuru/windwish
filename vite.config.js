d6f1400b568187c673bbf50986dfd67f715a1e0a
Jakodi Shi <jshi2019@gmail.com>
2026-07-14 20:45:01 -0500

    Fix paths

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     build: {
         outDir: 'docs',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash][extname]',
+            },
+        },
     },
+    base: './',
     plugins: [react()],
 });
